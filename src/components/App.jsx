5c0890a31ef6681105b970588d46275fc8218f94
Andres Gordon <gordon_andres@yahoo.es>
2022-06-30 23:41:12 -0500

    FIrst class project configuration
    Installing dependencies, configurating webpack, and adding some npm cmds

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <p>I'm a message from the react app, hello there.</p>
+      <h2>This is some other list</h2>
+      <ul>
+        <li>Hello</li>
+        <li>Some other message</li>
+        <li>I'm only waiting something</li>
+        <li>Bye I'm the las item</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
