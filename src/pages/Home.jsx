2ae270110e9b40a5e429bc7b21d1cb01db2240a8
Andres Gordon <gordon_andres@yahoo.es>
2022-10-14 16:06:08 -0500

    ✨ [ADD] Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Yardsale shop</h1>
+      <p>Welcome to the store of your dreams.</p>
+    </div>
+  );
+}
+
+export default NotFound;
