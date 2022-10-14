98a5e5b3fbb33a1cf53934f898473111a62a81c0
Andres Gordon <gordon_andres@yahoo.es>
2022-10-14 01:07:51 -0500

    💄 [ADD] Layout container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,13 @@
 import React from "react";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
 import '../styles/global.css';
 
 const App = () => {
   const view = (
-    <div className="main-container">
-      <h1>Hello world I'm using React to build this site!</h1>
-      <p>
-        I'm a message from the react app, hello there. I'm going to introduce myself, I'm Arcano a frontend developer who enjoys maths, logic and design as well.
-      </p>
-      <h2>This is some other list</h2>
-      <ul>
-        <li>Hello</li>
-        <li>Some other message</li>
-        <li>I'm only waiting something</li>
-        <li>Bye I'm the las item</li>
-      </ul>
-    </div>
+    <Layout>
+      <Login />
+    </Layout>
   );
 
   return view;
