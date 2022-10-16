914d1e08438db7e0a6f69bc895c39d15d6428ff6
Andres Gordon <gordon_andres@yahoo.es>
2022-10-16 16:50:30 -0500

    🩹 [FIX] Icon was not showing

--- src/components/MobileMenuButton.jsx
+++ src/components/MobileMenuButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "@styles/MobileMenuButton.scss";
+import hamIcon from "@icons/icon_menu.svg";
 
 const MobileMenuButton = () => {
   return (
     <a href="#" className="MobileMenuButton">
-      <img src="./assets/icons/icon_menu.svg" alt="Menu" />
+      <img src={hamIcon} alt="Menu" />
     </a>
   );
 }
