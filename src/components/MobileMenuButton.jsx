28af238454f353525c77ba2d35bbfa45c2583296
Andres Gordon <gordon_andres@yahoo.es>
2022-10-16 15:31:21 -0500

    ✨ [ADD] Mobile Menu Button component

---
+++ src/components/MobileMenuButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@styles/MobileMenuButton,scss";
+
+const MobileMenuButton = () => {
+  return (
+    <a href="#" className="mobile-menu-icon">
+      <img src="./assets/icons/icon_menu.svg" alt="Menu" />
+    </a>
+  );
+}
+
+export default MobileMenuButton;
