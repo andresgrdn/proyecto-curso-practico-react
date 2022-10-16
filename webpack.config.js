aba4d3f39f96541d2653e3c0615b460edaf58c9f
Andres Gordon <gordon_andres@yahoo.es>
2022-10-16 16:47:05 -0500

    🔧 [UPDATE] Add assets aliases

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,9 @@ module.exports = {
       '@components': path.resolve(__dirname, './src/components'),
       '@containers': path.resolve(__dirname, './src/containers'),
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@routes': path.resolve(__dirname, './src/routes')
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@icons': path.resolve(__dirname, './src/assets/icons'),
+      '@logos': path.resolve(__dirname, './src/assets/logos')
     }
   },
   module: {
