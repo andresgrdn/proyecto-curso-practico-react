75ea9b233e9d8fe65c3f80bdae246349676d58be
Andres Gordon <gordon_andres@yahoo.es>
2022-10-17 15:04:48 -0500

    ✨ [ADD] Products component

---
+++ src/containers/Products.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@styles/Products.scss";
+import "@styles/global.css";
+import ProductItem from "@components/ProductItem";
+
+const Products = () => {
+  return (
+    <div class="Products">
+      <ProductItem />
+    </div>
+  );
+}
+
+export default Products;
